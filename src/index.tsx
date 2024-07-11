bec834eabcdd658d2e84dc2e0b748422a4b99a3e
masuzuki1028 <test@gmail.com>
2024-07-12 07:52:33 +0900

    test

--- src/index.tsx
+++ src/index.tsx
@@ -4,16 +4,13 @@ import './destyle.css';
 // import './index.css';
 import App from './App';
 // import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router } from 'react-router-dom';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <Router basename="/react-todo-app">
     <App />
-    </Router>
   </React.StrictMode>
 );
 
